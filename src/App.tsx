7cbf6c1292ce04f2b4fb239ff96e64ae20815691
Pavel <sinelnikoff.p@gmail.com>
2024-01-20 17:33:33 +0300

    remove components

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
-          <Counter />
+         {/* <Counter />
           <TransferTon />
-          <Jetton />
+          <Jetton />*/}
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
